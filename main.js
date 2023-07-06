46e0cfe7f99c5cebf3b92c17023cbd20e2121644
Michelle Totherow <tym4me13@gmail.com>
2023-07-06 17:24:23 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const hailTheQueen = (nameString) => {
+    return `Hail Her Majesty, ${nameString}.`
+}
+
+const athena = hailTheQueen("Athena Perez")
+console.log(athena)
+
+const charisse = hailTheQueen("Charisse Ford")
+console.log(charisse)
+
+const jenna = hailTheQueen("Jenna Solis")
+console.log(jenna)
+
+const bob = hailTheQueen("Bob The Queen")
+console.log(bob)
+
+const mary = hailTheQueen("Mary Scotts")
+console.log(mary)
